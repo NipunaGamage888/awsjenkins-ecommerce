943a207ccdb01b68dda9df1803a5ba03cc1edf37
Nipunagamage888 <nipunagamage888@gmail.com>
2025-06-01 12:36:55 -0400

    pass

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'ecommerce-database.c0h2w6ciwaqa.us-east-1.rds.amazonaws.com',
   user: 'admin',
-  password: 'YOUR_PASSWORD_HERE',
+  password: 'Blahbleh12345?',
   database: 'ecommerce'
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-
 require('dotenv').config();
 const multer = require('multer');
 const AWS = require('aws-sdk');
